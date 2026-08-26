564935c8cd1698e484dd9168f91a7959b4a8531f
Abhishek Dixit <cphospitaldigitaldata@gmail.com>
2026-08-26 12:34:39 +0530

    Fix duplicate Pool declaration in server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 const session = require('express-session');
-require('dotenv').config();
 
 const app = express();
 app.use(express.static('public')); // (या जिस फोल्डर में आपका लोगो है, उसका नाम लिखें)
