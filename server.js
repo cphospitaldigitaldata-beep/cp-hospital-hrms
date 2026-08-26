bc9e940da84d78e3c086e7afd78f60e925870a77
Abhishek Dixit <cphospitaldigitaldata@gmail.com>
2026-08-26 12:27:12 +0530

    Fix express-session reference error in server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
